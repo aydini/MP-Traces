9c72fb2296e0f658d6ed806fe88fd3fabfcdc29b
aydini01 <aydini01@webserver.webserver-aydini.ch-geni-net.instageni.nysernet.org>
2021-08-03 12:57:35 -0400

    updated startSS.bash file

--- startSS.bash
+++ startSS.bash
@@ -1,8 +1,9 @@
-outDir=/mnt/MP-TRACE-FILES
+outDir=/mnt/mpTraceFiles # put no ending /
 outputFileName=`date +%F`-`date +%T`
 
+echo "starting ss and saving  output to ${outDir}/${outputFileName}.ss.txt"
 while true
 do 
-	ss --no-header -eipn dst :80 or src :80 | ts '%.S' | tee -a $outDir/${outputFileName}".ss.txt"
+	ss --no-header -eipn dst :80 or src :80 | ts '%.S' | tee -a "${outDir}/${outputFileName}.ss.txt"
 	sleep 0.1
 done
