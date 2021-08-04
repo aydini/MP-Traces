7e136ea7a5c12a4091ad37a72416f67dc99bf013
aydini01 <aydini01@webserver.webserver-aydini.ch-geni-net.instageni.nysernet.org>
2021-08-04 13:52:49 -0400

    updates to get the csv file for each tcp stream pcap file

--- analyzeData.bash
+++ analyzeData.bash
@@ -2,32 +2,56 @@
 
 
 # README: 
-# before running this script update the dir, file outdir variables
+# before running this script update the dir, serverIP
 #
 if [ $# -lt 1 ]
 then
-	echo; echo "usage:" ; echo " $0  pcapfilename"; echo
+	echo; echo "usage:" ; echo " $0  collectivePcapFileName"; echo
 else
-	dir=/mnt/mpTraceFiles #.pcap file director, put no ending /
-	file="$1" #.pcap file
-	outDir="${dir}/${file}-PROCESSED" # for individual TCP stream pcap files
-	sudo rm -rf $outDir; sudo mkdir $outDir
+	#----------------------------------------------------------------------
+	# get the pcap files per TCP stream inside the collectivePcapFileName
+	#-----------------------------------------------------------------------
+	#
+        dir=/mnt/mpTraceFiles #.pcap file director, put no ending /
+        serverIP="199.109.64.50"
+        file="$1" #.pcap file
+        outDir="${dir}/${file}-PROCESSED" # for individual TCP stream pcap files
+########sudo rm -rf $outDir; sudo mkdir $outDir
 
-	echo "analzing pcap file ${dir}/${file}..."
-	tshark -Tfields -e tcp.stream \
-			-e frame.time_epoch \
-			-e ip.src \
-			-e tcp.srcport \
-			-e ip.dst \
-			-e tcp.dstport -r "${dir}/${file}" |
-	  sort -snu |
-	  while read -a f; do 
-	  [[ "${f[5]}" ]] || continue  # sometimes there is no stream number ex. UDP
-	    fileout=$(echo ${f[0]}__${f[1]}__${f[2]}__${f[3]}__${f[4]}__${f[5]})
-	    tshark -r "${dir}/${file}" -2R "tcp.stream == ${f[0]}" -w "$fileout.pcap"
-	  done
+########echo "analzing pcap file ${dir}/${file}..."
+########tshark -Tfields -e tcp.stream \
+########		-e frame.time_epoch \
+########		-e ip.src \
+########		-e tcp.srcport \
+########		-e ip.dst \
+########		-e tcp.dstport -r "${dir}/${file}" |
+########  sort -snu |
+########  while read -a f; do 
+########  [[ "${f[5]}" ]] || continue  # sometimes there is no stream number ex. UDP
+########    fileout=$(echo ${f[0]}__${f[1]}__${f[2]}__${f[3]}__${f[4]}__${f[5]})
+########    tshark -r "${dir}/${file}" -2R "tcp.stream == ${f[0]}" -w "$fileout.pcap"
+########  done
 
-	  
-	sudo mv *__*pcap $outDir
-	echo "finished see the output files in ${outDir}"
+########  
+########sudo mv *__*pcap $outDir
+########echo "finished see TCP stream pcap files in ${outDir}"
+
+
+	#----------------------------------------------------------------------
+	# get the csv trace files per pcap files per TCP stream
+	#-----------------------------------------------------------------------
+	for streamFile in `ls $outDir/*.pcap`
+	do
+		echo "processing stream pcap file $streamFile to get csv file..."
+		tsharkFile="${streamFile}.tshark.txt"
+		csvFile="${streamFile}.csv"
+		sudo tshark -r "${streamFile}" -q -z io,stat,1,"BYTES()ip.src ==$serverIP" > $tsharkFile
+		# example input line 
+		#|   0 <>   1 |  9966648 |              |
+		cat $tsharkFile | grep "^| *[0-9]* *<> *[0-9]* *|" | tr -d ' <' | tr '>' '|' | awk -F"|" '{print $3","$4}' > $csvFile 
+	done
+   
+	echo "finished see the csv files in ${outDir}"
 fi
+
+
