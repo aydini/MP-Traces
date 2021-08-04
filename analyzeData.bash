c0d691bff9035c666daff9b87833268fd4a837a0
aydini01 <aydini01@webserver.webserver-aydini.ch-geni-net.instageni.nysernet.org>
2021-08-04 14:07:46 -0400

    updates to the analyzeData.bash file

--- analyzeData.bash
+++ analyzeData.bash
@@ -1,57 +1,66 @@
-# reference https://serverfault.com/questions/273066/tool-for-splitting-pcap-files-by-tcp-connection/881221#881221
+# Reference https://serverfault.com/questions/273066/tool-for-splitting-pcap-files-by-tcp-connection/881221#881221
+#Reference https://ask.wireshark.org/question/21680/how-to-export-tcp-throughput-into-csv-over-time/
 
 
 # README: 
-# before running this script update the dir, serverIP
+# before running this script update the dir, serverIP and flags
 #
+FLAG_GET_TCP_STREAM_PCAPS=0 # 1(true) or 0 (false)
+FLAG_GET_CSV_FILES=1 # 1(true) or 0 (false)
+dir=/mnt/mpTraceFiles #.pcap file director, put no ending /
+serverIP="199.109.64.50"
+        
 if [ $# -lt 1 ]
 then
 	echo; echo "usage:" ; echo " $0  collectivePcapFileName"; echo
 else
+	file="$1" #.pcap file
+        outDir="${dir}/${file}-PROCESSED" # for individual TCP stream pcap files
+
 	#----------------------------------------------------------------------
 	# get the pcap files per TCP stream inside the collectivePcapFileName
 	#-----------------------------------------------------------------------
-	#
-        dir=/mnt/mpTraceFiles #.pcap file director, put no ending /
-        serverIP="199.109.64.50"
-        file="$1" #.pcap file
-        outDir="${dir}/${file}-PROCESSED" # for individual TCP stream pcap files
-########sudo rm -rf $outDir; sudo mkdir $outDir
+	if [ $FLAG_GET_TCP_STREAM_PCAPS -eq 1 ]
+	then
+		sudo rm -rf $outDir; sudo mkdir $outDir
 
-########echo "analzing pcap file ${dir}/${file}..."
-########tshark -Tfields -e tcp.stream \
-########		-e frame.time_epoch \
-########		-e ip.src \
-########		-e tcp.srcport \
-########		-e ip.dst \
-########		-e tcp.dstport -r "${dir}/${file}" |
-########  sort -snu |
-########  while read -a f; do 
-########  [[ "${f[5]}" ]] || continue  # sometimes there is no stream number ex. UDP
-########    fileout=$(echo ${f[0]}__${f[1]}__${f[2]}__${f[3]}__${f[4]}__${f[5]})
-########    tshark -r "${dir}/${file}" -2R "tcp.stream == ${f[0]}" -w "$fileout.pcap"
-########  done
+		echo "analzing pcap file ${dir}/${file}..."
+		tshark -Tfields -e tcp.stream \
+				-e frame.time_epoch \
+				-e ip.src \
+				-e tcp.srcport \
+				-e ip.dst \
+				-e tcp.dstport -r "${dir}/${file}" |
+		  sort -snu |
+		  while read -a f; do 
+		  [[ "${f[5]}" ]] || continue  # sometimes there is no stream number ex. UDP
+		    fileout=$(echo ${f[0]}__${f[1]}__${f[2]}__${f[3]}__${f[4]}__${f[5]})
+		    tshark -r "${dir}/${file}" -2R "tcp.stream == ${f[0]}" -w "$fileout.pcap"
+		  done
 
-########  
-########sudo mv *__*pcap $outDir
-########echo "finished see TCP stream pcap files in ${outDir}"
-
+		  
+		sudo mv *__*pcap $outDir
+		echo "finished see TCP stream pcap files in ${outDir}"; echo " "
+	fi
 
 	#----------------------------------------------------------------------
 	# get the csv trace files per pcap files per TCP stream
 	#-----------------------------------------------------------------------
-	for streamFile in `ls $outDir/*.pcap`
-	do
-		echo "processing stream pcap file $streamFile to get csv file..."
-		tsharkFile="${streamFile}.tshark.txt"
-		csvFile="${streamFile}.csv"
-		sudo tshark -r "${streamFile}" -q -z io,stat,1,"BYTES()ip.src ==$serverIP" > $tsharkFile
-		# example input line 
-		#|   0 <>   1 |  9966648 |              |
-		cat $tsharkFile | grep "^| *[0-9]* *<> *[0-9]* *|" | tr -d ' <' | tr '>' '|' | awk -F"|" '{print $3","$4}' > $csvFile 
-	done
-   
-	echo "finished see the csv files in ${outDir}"
+	if [ $FLAG_GET_CSV_FILES -eq 1 ]
+	then	
+		for streamFile in `ls $outDir/3__*.pcap`
+		do
+			echo "processing stream pcap file $streamFile to get csv file..."
+			tsharkFile="${streamFile}.tshark.txt"
+			csvFile="${streamFile}.csv"
+			sudo tshark -r "${streamFile}" -q -z io,stat,1,"BYTES()ip.src ==$serverIP" > $tsharkFile
+			# example input line is as below and we create/output a time,throughput line
+			#|   0 <>   1 |  9966648 |              |
+			cat $tsharkFile | grep "^| *[0-9]* *<> *[0-9]* *|" | tr -d ' <' | tr '>' '|' | awk -F"|" '{print $3","$4}' > $csvFile 
+		done
+	   
+		echo "finished see the csv files in ${outDir}"; echo " "
+	fi	
 fi
 
 
