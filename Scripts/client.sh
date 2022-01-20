d882641f11e0f6eda96a019009faa0125186f5c1
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-01-20 12:31:12 -0500

    Update client.sh

--- Scripts/client.sh
+++ Scripts/client.sh
@@ -1,3 +1,34 @@
+# install packages
 sudo apt update
 sudo apt -y install iperf3
 sudo apt -y install moreutils
+
+# run sysctl commands to enable mptcp
+sudo sysctl -w net.mptcp.mptcp_enabled=1 
+sudo sysctl -w net.mptcp.mptcp_checksum=0
+
+# load and configure mptcp congestion control algorithms
+sudo modprobe mptcp_balia 
+sudo sysctl -w net.ipv4.tcp_congestion_control=balia
+
+# install iproute-mptcp package 
+sudo git clone https://github.com/multipath-tcp/iproute-mptcp.git
+cd iproute-mptcp 
+sudo make 
+sudo make install 
+cd
+
+######################################################
+# remove Cloudlab created automatically added routes
+######################################################
+iface1=$(ifconfig | grep -B1 "inet 192.168.10.2" | head -n1 | cut -f1 -d:)
+iface2=$(ifconfig | grep -B1 "inet 192.168.20.2" | head -n1 | cut -f1 -d:)
+
+# bring both interfaces of the client node down and then up
+sudo ifconfig $iface1 down; sudo ifconfig $iface1 up 
+sudo ifconfig $iface1 down; sudo ifconfig $iface1 up
+
+# add the new routes manually 
+sudo route add -net 192.168.3.0/24 gw 192.168.10.1 
+sudo route add -net 192.168.4.0/24 gw 192.168.20.1
+
