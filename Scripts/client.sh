d645963614ea7913fff632d4f8b16f00ce2a1213
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-01-28 09:26:21 -0500

    Update client.sh
    fix error: bring iface2 down and up

--- Scripts/client.sh
+++ Scripts/client.sh
@@ -30,7 +30,7 @@ ifaceC=$(ifconfig | grep -B1 "inet " | head -n1 | cut -f1 -d:)
 
 # remove Cloudlab created automatically added routes: bring both interfaces of the client node down and then up
 sudo ifconfig $iface1 down; sudo ifconfig $iface1 up 
-sudo ifconfig $iface1 down; sudo ifconfig $iface1 up
+sudo ifconfig $iface2 down; sudo ifconfig $iface2 up
 
 # add the new routes manually 
 sudo route add -net 192.168.3.0/24 gw 192.168.10.1 
