9a7ca432d31a7aa708055a8218985b4b2e51972c
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-01-28 09:37:54 -0500

    Update emulator1.sh
    rename iface1 as ifacetoC # interface connecting to client subnetwork

--- Scripts/emulator1.sh
+++ Scripts/emulator1.sh
@@ -1,11 +1,15 @@
 # get control and experiment interface names
-iface1=$(ifconfig | grep -B1 "inet 192.168.10.1" | head -n1 | cut -f1 -d:)
-iface2=$(ifconfig | grep -B1 "inet 192.168.1.1" | head -n1 | cut -f1 -d:)
+ifacetoC=$(ifconfig | grep -B1 "inet 192.168.10.1" | head -n1 | cut -f1 -d:)
+ifacetoS=$(ifconfig | grep -B1 "inet 192.168.1.1" | head -n1 | cut -f1 -d:)
 
 # remove Cloudlab created automatically added routes: bring both interfaces of the client node down and then up
-sudo ifconfig $iface1 down; sudo ifconfig $iface1 up 
-sudo ifconfig $iface2 down; sudo ifconfig $iface2 up
+sudo ifconfig $ifacetoC down; sudo ifconfig $ifacetoC up 
+sudo ifconfig $ifacetoS down; sudo ifconfig $ifacetoS up
 
 
 # add the new routes manually 
 sudo route add -net 192.168.3.0/24 gw 192.168.1.2
+
+# Introduce delay at the emulators 
+sudo tc qdisc replace dev $(ip route get 192.168.3.1 | grep -oP "(?<=dev )[^ ]+") root netem delay 30ms limit 60000
+
