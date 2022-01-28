6c5b3807dc622e540b597bf06484c45bbeb19676
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-01-28 10:12:53 -0500

    Update router1.sh

--- Scripts/router1.sh
+++ Scripts/router1.sh
@@ -8,3 +8,9 @@ sudo ifconfig $ifacetoS down; sudo ifconfig $ifacetoS up
 
 # add the new routes manually 
 sudo route add -net 192.168.10.0/24 gw 192.168.1.1 
+
+# Set bottleneck capacity and buffer size at the router
+sudo tc qdisc del dev $ifacetoS root # don’t worry if you get RTNETLINK  error
+sudo tc qdisc replace dev $ifacetoS root handle 1: htb default 3 
+sudo tc class add dev $ifacetoS parent 1: classid 1:3 htb rate 100mbit 
+sudo tc qdisc add dev $ifacetoS parent 1:3 handle 3: bfifo limit 375000 
