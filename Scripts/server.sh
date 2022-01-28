fe6edf1c75ca4f289cc251207ed75d3f836f4c37
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-01-28 09:28:24 -0500

    Update server.sh
    fix error: bring iface2 down and up

--- Scripts/server.sh
+++ Scripts/server.sh
@@ -20,7 +20,7 @@ ifaceC=$(ifconfig | grep -B1 "inet " | head -n1 | cut -f1 -d:)
 
 # bring both interfaces of the client node down and then up
 sudo ifconfig $iface1 down; sudo ifconfig $iface1 up 
-sudo ifconfig $iface1 down; sudo ifconfig $iface1 up
+sudo ifconfig $iface2 down; sudo ifconfig $iface2 up
 
 # add the new routes manually 
 sudo route add -net 192.168.10.0/24 gw 192.168.3.2 
