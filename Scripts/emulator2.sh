ef547354d3731299f084f06af82fd08ef1312658
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-01-28 09:25:09 -0500

    Create emulator2.sh

---
+++ Scripts/emulator2.sh
@@ -0,0 +1,11 @@
+# get control and experiment interface names
+iface1=$(ifconfig | grep -B1 "inet 192.168.20.1" | head -n1 | cut -f1 -d:)
+iface2=$(ifconfig | grep -B1 "inet 192.168.2.1" | head -n1 | cut -f1 -d:)
+
+# remove Cloudlab created automatically added routes: bring both interfaces of the client node down and then up
+sudo ifconfig $iface1 down; sudo ifconfig $iface1 up 
+sudo ifconfig $iface2 down; sudo ifconfig $iface2 up
+
+
+# add the new routes manually 
+sudo route add -net 192.168.4.0/24 gw 192.168.2.2
