8078017784f582c21c80dc55b07623afcd844dbd
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-03-05 14:12:30 -0500

    Update serverWebBrowsing.sh

--- Scripts/serverWebBrowsing.sh
+++ Scripts/serverWebBrowsing.sh
@@ -1,5 +1,5 @@
 # $1: name of the output file to record wget statistics
-dir='/var/www/html/webBrowsingExperimentData/' # to store the web browsing data
+dir='./webBrowsingExperimentData/' # to store the web browsing data
 ip=192.168.10.2 # ip1 of the apache web server (client)
 outputFile="$1"  # pick names like p{PathNo}t{TrialNo}config where config will be nothing, C for contant, M for memoryless random distribution example: p7t1  p7t1C p7t1M
 
