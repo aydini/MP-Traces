6f2ce6cfceb35c05a340d0070695b5155fab3d33
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-03-08 12:15:22 -0500

    Update serverWebBrowsing.sh

--- Scripts/serverWebBrowsing.sh
+++ Scripts/serverWebBrowsing.sh
@@ -4,7 +4,7 @@ ip=192.168.10.2 # ip1 of the apache web server (client)
 outputFile="$1"  # pick names like p{PathNo}t{TrialNo}config where config will be nothing, C for contant, M for memoryless random distribution example: p7t1  p7t1C p7t1M
 
 while true; do
-for url in {'engineering.nyu.edu','reddit.com'}; do                                                                        
+for url in {'engineering.nyu.edu','reddit.com','facebook.com','youtube.com','mlb.com/mets'}; do                                                                        
         # last part of wget output
         #       Total wall clock time: 5.2s
         # output line format is: url,timeInSeconds
