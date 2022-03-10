c36714839a506829e669dd37f39375645af65851
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-03-10 02:51:24 -0500

    Update client.sh

--- Scripts/client.sh
+++ Scripts/client.sh
@@ -54,6 +54,6 @@ sudo ip route add 192.168.4.0/24 via 192.168.20.1 dev $iface2 table 2
 cd /var/www/html/
 sudo wget -e robots=off --wait 1 -H -p -k http://engineering.nyu.edu/
 sudo wget -e robots=off --wait 1 -H -p -k http://reddit.com/
-sudo wget -e robots=off --wait 1 -H -p -k http://facebook.com/  
+sudo wget -e robots=off --wait 1 -H -p -k http://weather.com/  
 sudo wget -e robots=off --wait 1 -H -p -k http://youtube.com/  
 sudo wget -e robots=off --wait 1 -H -p -k http://farmingdale.edu/
