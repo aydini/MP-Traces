11c0adbb294e4a96cc4de5ee167e73d8c3b7d61e
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-03-11 16:39:46 -0500

    Add files via upload

---
+++ Scripts/startServerWebBrowsing.sh
@@ -0,0 +1,19 @@
+# $1: name of the output file to record wget statistics
+dir='./testWebBrowsingExperimentData/' # to store the web browsing data
+ip=192.168.10.2 # ip1 of the apache web server (client)
+outputFile="$1"  # pick names like p{PathNo}t{TrialNo}config where config will be nothing, C for contant, M for memoryless random distribution example: p7t1  p7t1C p7t1M
+
+mkdir -p $dir
+counter=0
+while true; do
+for url in {'engineering.nyu.edu','reddit.com','weather.com','youtube.com','farmingdale.edu'}; do                                                                        
+        let counter=$counter+1
+	echo "$counter $(date)"
+	outputFile="${counter}_${1}"
+	# last part of wget output
+        #       Total wall clock time: 5.2s
+        # output line format is: url,timeInSeconds
+        wget -o "${dir}${outputFile}" -p "http://${ip}/${url}/" &
+        sleep 10
+done                                                                                                            
+done
