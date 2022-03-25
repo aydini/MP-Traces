4f0c5db02c312dcf5bb3e88f6d34e565f0ee5e34
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-03-25 10:09:18 -0400

    Update startServerWebBrowsing.sh

--- Scripts/startServerWebBrowsing.sh
+++ Scripts/startServerWebBrowsing.sh
@@ -1,7 +1,7 @@
 # $1: name of the output file to record wget statistics
 dir='./testWebBrowsingExperimentData/' # to store the web browsing data
 ip=192.168.10.2 # ip1 of the apache web server (client)
-outputFile="$1"  # pick names like p{PathNo}t{TrialNo}config where config will be nothing, C for contant, M for memoryless random distribution example: p7t1  p7t1C p7t1M
+outputFile="$1"  # pick names like p{PathNo}t{TrialNo}config where config will be nothing, C for constant, M for memoryless random distribution example: p7t1  p7t1C p7t1M
 
 mkdir -p $dir
 counter=0
