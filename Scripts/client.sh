9ee924119656ed592ba5f411ff57d8dd072ec3a7
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2022-04-01 04:44:01 -0400

    Update client.sh

--- Scripts/client.sh
+++ Scripts/client.sh
@@ -55,5 +55,5 @@ cd /var/www/html/
 sudo wget -e robots=off --wait 1 -H -p -k http://engineering.nyu.edu/
 sudo wget -e robots=off --wait 1 -H -p -k http://reddit.com/
 sudo wget -e robots=off --wait 1 -H -p -k http://weather.com/  
-sudo wget -e robots=off --wait 1 -H -p -k http://youtube.com/  
+sudo wget -e robots=off --wait 1 -H -p -k http://wikipedia.com/  
 sudo wget -e robots=off --wait 1 -H -p -k http://farmingdale.edu/
