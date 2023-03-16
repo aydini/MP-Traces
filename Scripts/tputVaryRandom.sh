10ad295d28783f568825f1d46fc3865c16e974f7
Ilknur Aydin <aydini@farmingdale.edu>
2023-03-16 17:42:30 +0000

    started cleaning up the notebook

--- Scripts/tputVaryRandom.sh
+++ Scripts/tputVaryRandom.sh
@@ -1,14 +1,20 @@
 # run this script at each router to set up the varying bandwidth from a (random) memoryless process
 # $1: mean data rate
-# $2: standard deviation of the data rate
+# $2: standard deviation of the data rate - if this is zero then the static (constant) data rate is used.
 
 #!/bin/bash
 
 ifacetoS=$(ifconfig | grep -B1 "inet 192.168.3.2\|inet 192.168.4.2" | head -n1 | cut -f1 -d:)
 
 while true; do
-  tputmbit=$(python3 -c "import random; print(random.gauss($1, $2))")
+  if (( $2 == "0" )); then
+      tputmbit=$1
+  else  
+      tputmbit=$(python3 -c "import random; print(random.gauss($1, $2))")      
+  fi
+  
   tputkbit=$( bc <<< "scale=2; $tputmbit*1000" )   
+  
   if (( $(echo "0 > $tputkbit" |bc -l) )); then  
     tputkbit=1
   fi
