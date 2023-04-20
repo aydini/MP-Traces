7b8010e6cb77133e61d2b6ce2d8a70848ce5e02b
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2023-04-20 13:08:17 -0400

    Update startTcpdump.bash
    getting the public IP address of the web serverIP

--- startTcpdump.bash
+++ startTcpdump.bash
@@ -1,5 +1,5 @@
 outDir=/mnt/mpTraceFiles
-outputFileName=`date +%F`-`date +%T` 
+outputFileName=$(ip route get 8.8.8.8 | awk '{print $7}' | head -n 1)
 serverIP="199.109.64.50"
 echo "starting tcpdump and saving output to ${outDir}/${outputFileName}.pcap"
 #sudo tcpdump host $serverIP and port 80 -s 66 -w "${outDir}/${outputFileName}.pcap"
